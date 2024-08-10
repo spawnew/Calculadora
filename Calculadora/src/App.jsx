71f5965e4f0209f0fb3d0d2775cc80b8073429fe
spawnew <96084099+spawnew@users.noreply.github.com>
2024-08-10 17:24:49 -0300

    Initial commit

---
+++ Calculadora/src/App.jsx
@@ -0,0 +1,35 @@
+
+import Boton from './Componentes/Boton'
+import './App.css'
+
+function App() {
+
+
+  return (
+    
+      <div className='container'>
+        <div className='pantalla'>
+        <div className='botones1'>
+        <Boton {9} />
+          
+        
+        <Boton>
+          8
+        </Boton>
+        <Boton>
+          7
+        </Boton>
+        </div>
+        <div className='botones2'>
+        
+        </div>
+        <div className='botones3'>
+        
+        </div>
+        </div>
+        </div>
+    
+  )
+}
+
+export default App
---
+++ Calculadora/src/Componentes/Boton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Boton = ({numero}) => {
+  return (
+    <div>
+      <h1>{numero}</h1>
+    </div>
+  )
+}
+
+export default Boton;
